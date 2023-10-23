6a58563b1426fe6d9f2c87a0ee83f7236e6482d2
Ricardo <ricardo.remond.pro@gmail.com>
2023-10-23 12:58:33 +0200

    commit

---
+++ scprit.js
@@ -0,0 +1,15 @@
+class abonnés {
+    constructor(id, photo, pseudo, age, sexe, orientation, departement, description) {
+        this.id = id
+        this.pseudo = pseudo
+        this.age = age
+        this.sexe = sexe
+        this.orientation = orientation
+        this.departement = departement
+        this.description = description
+    }
+}
+
+let michel = new abonnés("37", "Michel05", "31", "Homme", "Femme", "76", "A la recherche du grand amour",)
+let paul = new abonnés("38", "PauloRAFF", "24", "Homme", "Femme", "76", " Qui pour Netflix & Chill ? ")
+let Medhi = new abonnés("39", "MedhiA14H", "21", "Homme","Femme","38","salut ça va ? ")
