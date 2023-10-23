94dabd63a3ef66f93e8185ae888797b0dbc6539a
Ricardo <ricardo.remond.pro@gmail.com>
2023-10-23 13:24:52 +0200

    commit

---
+++ script.js
@@ -0,0 +1,26 @@
+class abonnés {
+    constructor(id, photo, pseudo, age, sexe, orientation, departement, description) {
+        this.id = id
+        this.photo = photo
+        this.pseudo = pseudo
+        this.age = age
+        this.sexe = sexe
+        this.orientation = orientation
+        this.departement = departement
+        this.description = description
+    }
+}
+
+let michel = new abonnés("37", "Michel05", "31", "Homme", "Femme", "76", "A la recherche du grand amour",)
+let paul = new abonnés("38", "PauloRAFF", "24", "Homme", "Femme", "76", " Qui pour Netflix & Chill ? ")
+let medhi = new abonnés("39", "MedhiA14H", "21", "Homme", "Femme", "38", "salut ça va ? ")
+let fabien = new abonnés("40", "MMFabien", "27", "Homme", "Femme", "76", "J'aime la bouffe ")
+let karim = new abonnés("41", "KB9", "19", "Homme", "Femme", "38", "J'esper taim le foot")
+let sami = new abonnés("42", "MedhiA14H", "21", "Homme", "Femme", "38", "salut ça va ? ")
+
+let marie = new abonnés ("43","","Marie")
+let lisa =
+let claire =
+let fati  =
+let fanta =
+let assia =
